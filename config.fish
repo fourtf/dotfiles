a73fe817bc4cea47306d994630a241ebacf698fb
fourtf <tf.four@gmail.com>
2019-09-16 20:21:38 +0200

    added wc aliases

--- config.fish
+++ config.fish
@@ -18,7 +18,13 @@ end
 
 source ~/.config/fish/local.fish
 
+alias count-lines "wc -l"
+alias count-bytes "wc -c"
+alias count-chars "wc -m"
+alias count-words "wc -w"
 alias gap "git add --patch ."
+alias listen-port "nc -lvp"
+
 
 set PATH ~/.local/bin $PATH
 set -x LS_COLORS "$LS_COLORS:ow=1;34:tw=1;34:"
